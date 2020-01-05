9c82730aa4502f291535f0c7b8824cb379dc8fd4
Dubois Matthieu <mdubois@intermade.lu>
2020-01-05 12:25:34 +0100

    Init portfolio

---
+++ precache-manifest.3d9b7b5dfc507e58f147298651c93f46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad57ad19b3dc16f13eb2",
+    "url": "/static/css/main.4c4f3b0e.chunk.css"
+  },
+  {
+    "revision": "ad57ad19b3dc16f13eb2",
+    "url": "/static/js/main.ad57ad19.chunk.js"
+  },
+  {
+    "revision": "bacdc14b25bc5e8a25ef",
+    "url": "/static/js/1.bacdc14b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8fa6dc48a21663687f973f5cb8b1c111",
+    "url": "/static/media/CeraPro-Regular.8fa6dc48.woff"
+  },
+  {
+    "revision": "a9821ec431598f380d926e5528425e89",
+    "url": "/static/media/CeraPro-Light.a9821ec4.woff"
+  },
+  {
+    "revision": "808440a0a16864b0e8a6f9659217c7e6",
+    "url": "/static/media/CeraPro-Bold.808440a0.woff"
+  },
+  {
+    "revision": "82a015d77f3f32647be002f5ca850c13",
+    "url": "/static/media/greyBackground-1.82a015d7.jpg"
+  },
+  {
+    "revision": "990dd0f566b961921cfc51a7c1ac8783",
+    "url": "/static/media/black-portrait.990dd0f5.png"
+  },
+  {
+    "revision": "a841b40ddff8aad696a908c6f1cd7a2f",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.b50df60e2961db601a3546951ea1e1c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "21628d53ba2255a80e9a",
+    "url": "/static/css/main.f4fe6889.chunk.css"
+  },
+  {
+    "revision": "21628d53ba2255a80e9a",
+    "url": "/static/js/main.21628d53.chunk.js"
+  },
+  {
+    "revision": "4b5f60eb09947c665bb0",
+    "url": "/static/js/1.4b5f60eb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5620baa03685816c014c261e9bd856cd",
+    "url": "/static/media/logo.5620baa0.png"
+  },
+  {
+    "revision": "8fa6dc48a21663687f973f5cb8b1c111",
+    "url": "/static/media/CeraPro-Regular.8fa6dc48.woff"
+  },
+  {
+    "revision": "808440a0a16864b0e8a6f9659217c7e6",
+    "url": "/static/media/CeraPro-Bold.808440a0.woff"
+  },
+  {
+    "revision": "a9821ec431598f380d926e5528425e89",
+    "url": "/static/media/CeraPro-Light.a9821ec4.woff"
+  },
+  {
+    "revision": "82a015d77f3f32647be002f5ca850c13",
+    "url": "/static/media/greyBackground-1.82a015d7.jpg"
+  },
+  {
+    "revision": "990dd0f566b961921cfc51a7c1ac8783",
+    "url": "/static/media/black-portrait.990dd0f5.png"
+  },
+  {
+    "revision": "93789ad2c0080595ba3f02beea70e6a4",
+    "url": "/index.html"
+  }
+];
